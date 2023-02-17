8cc2f586294dbd622d15083d6640ab23c713dc72
fulcanelly <dallasxfulcanelly@gmail.com>
2023-02-18 01:19:00 +0200

    refactor loader

--- cli/auto-loader.rb
+++ cli/auto-loader.rb
@@ -9,52 +9,50 @@ module SpigetApi
 end
 
 class << SpigetApi
-	def get_conn
-		uri = URI"https://api.spiget.org/"
-		Net::HTTP.start(uri.host, uri.port, :use_ssl => true)
-	end
+  def get_conn
+    uri = URI"https://api.spiget.org/"
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => true)
+  end
 
-	def make_request(url)
-		pp url
-		get_conn().request(Net::HTTP::Get.new(url)).body
-	end
+  def make_request(url)
+    pp url
+    get_conn().request(Net::HTTP::Get.new(url)).body
+  end
 
-	def make_json_req(url)
-		make_request(url)
-			.then do
-				JSON.load _1
-			end
-	end
+  def make_json_req(url)
+    make_request(url)
+      .then do
+        JSON.load _1
+      end
+  end
 
-	def last_version(res)
-		"https://api.spiget.org/v2/resources/8/versions/latest"
-		make_json_req("https://api.spiget.org/v2/resources/#{res}/versions/latest")
-	end
+  def last_version(res)
+    make_json_req("https://api.spiget.org/v2/resources/#{res}/versions/latest")
+  end
 
-	def get_versions(res)
-		make_json_req("https://api.spiget.org/v2/resources/#{res}/versions")
-	end
+  def get_versions(res)
+    make_json_req("https://api.spiget.org/v2/resources/#{res}/versions")
+  end
 
-	def download_by_id(res)
-		res = make_request("https://api.spiget.org/v2/resources/#{res}/download")
-		matched = res.match /https:.*/
-        if matched then
-			OpenURI.open_uri(matched[0])
-        else
-        	throw "not found"
-        end
+  def download_by_id(res)
+    res = make_request("https://api.spiget.org/v2/resources/#{res}/download")
+    matched = res.match /https:.*/
+    if matched then
+      OpenURI.open_uri(matched[0])
+    else
+      throw "not found"
+    end
+  end
 
-	end
+  def get_details(res_id)
+    make_json_req("https://api.spiget.org/v2/resources/#{res_id}")
+  end
 
-	def get_details(res_id)
-		make_json_req("https://api.spiget.org/v2/resources/#{res_id}")
-	end
-
-	def search_by_name(name)
-	 	make_json_req("https://api.spiget.org/v2/search/resources/#{
-	 		CGI.escape(name)
-		}?field=name&size=2")
-	end
+  def search_by_name(name)
+    make_json_req("https://api.spiget.org/v2/search/resources/#{
+      CGI.escape(name)
+    }?field=name&size=2")
+  end
 end
 
 
@@ -63,85 +61,57 @@ end
 # ways to supply pluging:
 # by name, by spigot id, by direct url
 class Plugin
+  def validate()
+  end
 
-	#should raise exception or load plugin from spigot
-	def self::parse(entry)
-		if entry["name"] then
+  def load_name
+  end
 
-			#SpigetApi.download_by_id()
-			#pp plugin["File"]["type"]
-			if plugin then
-				pp plugin["name"]
-			else
-				pp entry
-				puts "!!!\n"
-			end
+  def load_versions
+  end
 
-		else
-			throw "bad plugin config"
-		end
-		puts
+  #loads plugin to cache
+  def load()
+  end
 
-
-	end
-
-	def validate()
-	end
-
-	def load_name
-	end
-
-	def load_versions
-	end
-
-	#loads plugin to cache
-	def load()
-	end
-
-	# gives loaction of plugin
-	def get_file()
-	end
-
+  # gives loaction of plugin
+  def get_file()
+  end
 end
 
-
 config = YAML.load(
-	File.read('config.yml')
+  File.read('config.yml')
 )
 
 def load_plugins(config)
-	config["plugins"].each do |key, entry|
+  config["plugins"].each do |key, entry|
+    # find plugin
+    plugins = SpigetApi.search_by_name(entry["name"])
 
-		# find plugin
-		plugins = SpigetApi.search_by_name(entry["name"])
+    # first match
+    plugin = plugins.first
+    id = plugin["id"]
 
-		# first match
-		plugin = plugins.first
-		id = plugin["id"]
+    # get latest version
+    last = SpigetApi.last_version(id)
+    version_name = last["name"]
 
-		# get latest version
-		last = SpigetApi.last_version(id)
-		version_name = last["name"]
+    # get file name
+    name = plugin["name"].split(/\s/).first
 
-		# get file name
-		#details = SpigetApi.get_details(last["resource"])
+    file_name = "out/#{name}-#{version_name}.jar"
+    puts file_name.red
 
-		name = plugin["name"].split(/\s/).first
+    Dir.mkdir('out') unless Dir.exists?('out')
 
-		file_name = "out/#{name}-#{version_name}.jar"
+    # download
+    open(file_name, 'w') do |out|
+        file = SpigetApi.download_by_id(last["resource"])
+        out.write(file.read)
+    end
 
-		puts file_name.red
-
-		Dir.mkdir('out') unless Dir.exists?('out')
-
-		# download
-		open(file_name, 'w') do |out|
-			file = SpigetApi.download_by_id(last["resource"])
-			out.write(file.read)
-		end
-
-		puts
-	end
+    puts
+end
 
 
 end
