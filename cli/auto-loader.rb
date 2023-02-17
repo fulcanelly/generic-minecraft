ad3a88498569994c7a78e2e547948bad6bff1d06
fulcanelly <dallasxfulcanelly@gmail.com>
2023-02-18 00:48:42 +0200

    fut: implement plugin loading

--- cli/auto-loader.rb
+++ cli/auto-loader.rb
@@ -3,16 +3,15 @@ require 'json'
 require 'open-uri'
 require 'net/http'
 require 'cgi'
+require 'colored'
 
 module SpigetApi
 end
 
 class << SpigetApi
-
-
 	def get_conn
 		uri = URI"https://api.spiget.org/"
-		Net::HTTP.start(uri.host, uri.port, :use_ssl => true) 
+		Net::HTTP.start(uri.host, uri.port, :use_ssl => true)
 	end
 
 	def make_request(url)
@@ -22,17 +21,15 @@ class << SpigetApi
 
 	def make_json_req(url)
 		make_request(url)
-			.then do 
+			.then do
 				JSON.load _1
 			end
 	end
 
-	
 	def last_version(res)
 		"https://api.spiget.org/v2/resources/8/versions/latest"
 		make_json_req("https://api.spiget.org/v2/resources/#{res}/versions/latest")
 	end
-
 
 	def get_versions(res)
 		make_json_req("https://api.spiget.org/v2/resources/#{res}/versions")
@@ -41,56 +38,67 @@ class << SpigetApi
 	def download_by_id(res)
 		res = make_request("https://api.spiget.org/v2/resources/#{res}/download")
 		matched = res.match /https:.*/
-        if matched then 
+        if matched then
 			OpenURI.open_uri(matched[0])
-        else 
+        else
         	throw "not found"
         end
 
 	end
 
+	def get_details(res_id)
+		make_json_req("https://api.spiget.org/v2/resources/#{res_id}")
+	end
 
-	def search_by_name(name)	 
+	def search_by_name(name)
 	 	make_json_req("https://api.spiget.org/v2/search/resources/#{
 	 		CGI.escape(name)
 		}?field=name&size=2")
 	end
-
 end
 
 
 #pp SpigetApi.download_by_id(2333333333332)
-
+#flow (validate) -> (load info) -> (load file) -> applly
 # ways to supply pluging:
 # by name, by spigot id, by direct url
-class Plugin 
-	
-	#should raise exception or load plugin from spigot 
+class Plugin
+
+	#should raise exception or load plugin from spigot
 	def self::parse(entry)
-		if entry["name"] then 
-			plugin = SpigetApi.search_by_name(entry["name"])
-			pp plugin
-			plugin = plugin.first
+		if entry["name"] then
+
+			#SpigetApi.download_by_id()
 			#pp plugin["File"]["type"]
-			if plugin then 
+			if plugin then
 				pp plugin["name"]
-			else 
-				pp entry 
+			else
+				pp entry
 				puts "!!!\n"
 			end
 
-		else 
+		else
 			throw "bad plugin config"
 		end
+		puts
 
 
 	end
 
-	#loads plugin to cache 
+	def validate()
+	end
+
+	def load_name
+	end
+
+	def load_versions
+	end
+
+	#loads plugin to cache
 	def load()
 	end
 
-	# gives loaction of plugin 
+	# gives loaction of plugin
 	def get_file()
 	end
 
@@ -101,14 +109,41 @@ config = YAML.load(
 	File.read('config.yml')
 )
 
-def parse_plugins(config)
+def load_plugins(config)
 	config["plugins"].each do |key, entry|
-		Plugin.parse(entry)
-	
+
+		# find plugin
+		plugins = SpigetApi.search_by_name(entry["name"])
+
+		# first match
+		plugin = plugins.first
+		id = plugin["id"]
+
+		# get latest version
+		last = SpigetApi.last_version(id)
+		version_name = last["name"]
+
+		# get file name
+		#details = SpigetApi.get_details(last["resource"])
+
+		name = plugin["name"].split(/\s/).first
+
+		file_name = "out/#{name}-#{version_name}.jar"
+
+		puts file_name.red
+
+		Dir.mkdir('out') unless Dir.exists?('out')
+
+		# download
+		open(file_name, 'w') do |out|
+			file = SpigetApi.download_by_id(last["resource"])
+			out.write(file.read)
+		end
+
+		puts
 	end
 
 
 end
 
-pp config
-parse_plugins(config)
+load_plugins(config)
